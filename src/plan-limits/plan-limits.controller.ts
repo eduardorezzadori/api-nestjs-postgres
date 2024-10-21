e1d5a1190958a64531d7e7e76f4493688d8d9995
Eduardo Rezzadori <r.eduardo.23@outlook.com>
2024-10-20 22:01:27 -0300

    chore: finalização do CRUD de limites dos planos

--- src/plan-limits/plan-limits.controller.ts
+++ src/plan-limits/plan-limits.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
 import { CreatePlanLimitsDto } from './dto/create-plan-limits.dto';
 import { PlanLimitsService } from './plan-limits.service';
+import { PlanLimits } from '@prisma/client';
 
 @Controller('plan-limits')
 export class PlanLimitsController {
@@ -9,5 +10,25 @@ export class PlanLimitsController {
     @Post()
     async create(@Body() createPlanLimitsDto: CreatePlanLimitsDto) {
         return this.planLimitsService.create(createPlanLimitsDto);
+    }
+
+    @Get(':id')
+    async find(@Param('id') id: string): Promise<PlanLimits> {
+        return this.planLimitsService.find(id);
+    }
+
+    @Get()
+    async findAll(): Promise<PlanLimits[]> {
+        return this.planLimitsService.findAll();
+    }
+
+    @Patch(':id')
+    async update(@Param('id') id: string, @Body() createPlanLimitsDto: CreatePlanLimitsDto): Promise<PlanLimits> {
+        return this.planLimitsService.update(id, createPlanLimitsDto);
+    }
+
+    @Delete(':id')
+    async remove(@Param('id') id: string): Promise<string> {
+        return this.planLimitsService.remove(id);
     }
 }
--- src/plan-limits/plan-limits.service.ts
+++ src/plan-limits/plan-limits.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { PlanLimits } from '@prisma/client';
 import { CreatePlanLimitsDto } from './dto/create-plan-limits.dto';
@@ -7,9 +7,65 @@ import { CreatePlanLimitsDto } from './dto/create-plan-limits.dto';
 export class PlanLimitsService {
     constructor(private prisma: PrismaService) { }
 
-    async create(createPlanLimitsDto: CreatePlanLimitsDto) {
+    async create(createPlanLimitsDto: CreatePlanLimitsDto): Promise<PlanLimits> {
         return this.prisma.planLimits.create({
             data: createPlanLimitsDto,
         });
     }
+
+    async find(id: string): Promise<PlanLimits> {
+        return this.prisma.planLimits.findUnique({
+            where: { id: id }
+        });
+    }
+
+    async findAll(): Promise<PlanLimits[]> {
+        return this.prisma.planLimits.findMany({});
+    }
+
+    async update(id: string, createPlanLimitsDto: CreatePlanLimitsDto): Promise<PlanLimits> {
+        const { kilowatts, instalations } = createPlanLimitsDto;
+
+        try {
+            const foundLimits = await this.prisma.planLimits.findFirst({
+                where: { id: id }
+            });
+
+            if (!foundLimits) { throw new NotFoundException('Os limites do plano não foram encontrados!'); }
+
+            const updatePlanLimits = this.prisma.planLimits.update({
+                where: { id: id },
+                data: {
+                    kilowatts: kilowatts ? kilowatts : foundLimits.kilowatts,
+                    instalations: instalations ? instalations : foundLimits.instalations,
+                },
+            });
+
+            return updatePlanLimits;
+
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async remove(id: string) {
+        try {
+            const foundLimits = await this.prisma.planLimits.findUnique({
+                where: { id: id }
+            });
+
+            if (!foundLimits) throw new NotFoundException('Os limites do plano não foram encontrados!');
+
+            const removedPlanLimits = await this.prisma.planLimits.delete({
+                where: { id: id }
+            });
+
+            return 'Os limites do plano foram deletados com sucesso!';
+
+        } catch (error) {
+            throw error;
+        }
+    }
+
+
 }
