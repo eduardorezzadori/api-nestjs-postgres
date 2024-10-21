d77453b5bd4aecf1bcb39ef6d2e4d0997e9a45cb
Eduardo Rezzadori <r.eduardo.23@outlook.com>
2024-10-20 22:53:51 -0300

    chore: criação do módulo de usuários

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { PrismaModule } from './prisma/prisma.module';
 import { PlanModule } from './plan/plan.module';
 import { PrismaService } from './prisma/prisma.service';
 import { PlanLimitsModule } from './plan-limits/plan-limits.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [PrismaModule, PlanModule, PlanLimitsModule],
+  imports: [PrismaModule, PlanModule, PlanLimitsModule, UsersModule],
   controllers: [AppController],
   providers: [AppService]
 })
